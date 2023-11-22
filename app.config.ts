e2f548c80e940a890e02c3bd2eca4ae188892c25
nyxb <contact@nyxb.email>
2023-11-22 02:53:11 +0100

    ✨ feat(app.config.ts): update app configuration for NYXBCommands documentation
    The commit updates the application configuration to reflect the new context of the NYXBCommands project, including updated project title, description, image, and GitHub repository details. This change is intended to provide accurate and relevant information for the NYXBCommands documentation site.

--- app.config.ts
+++ app.config.ts
@@ -1,38 +1,38 @@
 export default defineAppConfig({
-   docux: {
-      title: 'Docux',
-      description: 'The best place to start your documentation.',
-      image: 'https://github.com/nyxb/docux/blob/main/.github/images/docux_repo_picture.png',
-      socials: {
-         twitter: 'nyxb0',
-         github: 'nyxb/docux',
-      },
-      github: {
-         dir: '.starters/default/content',
-         branch: 'main',
-         repo: 'docux',
-         owner: 'nyxb',
-         edit: true,
-      },
-      aside: {
-         level: 1,
-         collapsed: false,
-         exclude: [],
-         navigation: true,
-      },
-      main: {
-         padded: true,
-      },
-      header: {
-         title: '',
-         logo: true,
-         showLinkIcon: true,
-         exclude: [],
-         navigation: false,
-      },
-      footer: {
-         iconLinks: [
-         ],
-      },
-   },
+  docux: {
+    title: 'NYXBCommands',
+    description: 'NYXBCommands is a Discord.JS command handler developed by Nyxb. It\'s designed to simplify your bot development, allowing you to focus on creating unique commands and functionalities without worrying about the underlying command handling architecture.',
+    image: 'https://raw.githubusercontent.com/nyxb/nyxbcommands.docs/main/public/NyxbShot-2023-11-22-at-02.50.06%402x.png',
+    socials: {
+      twitter: 'nyxb0',
+      github: 'nyxb/nyxbcommands.docs',
+    },
+    github: {
+      dir: 'content',
+      branch: 'main',
+      repo: 'nyxbcommands.docs',
+      owner: 'nyxb',
+      edit: true,
+    },
+    aside: {
+      level: 1,
+      collapsed: false,
+      exclude: [],
+      navigation: true,
+    },
+    main: {
+      padded: true,
+    },
+    header: {
+      title: '',
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+      navigation: false,
+    },
+    footer: {
+      iconLinks: [
+      ],
+    },
+  },
 })
